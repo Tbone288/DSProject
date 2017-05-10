018bd29c7abb28497e63fe529b008f58f95e054a
Tbone288 <tylerodom2@gmail.com>
2017-05-10 12:19:41 -0600

    Created Data Structures project.

---
+++ src/ds/controller/DSController.java
@@ -0,0 +1,5 @@
+package ds.controller;
+
+public class DSController {
+
+}
---
+++ src/ds/controller/DSRunner.java
@@ -0,0 +1,10 @@
+package ds.controller;
+
+public class DSRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
